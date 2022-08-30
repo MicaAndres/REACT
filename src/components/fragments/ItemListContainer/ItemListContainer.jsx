2c1977bb0fc63180e5c725c5ca9f2034fac3757a
Micaela Andres <micaailenandres@gmail.com>
2022-08-30 13:38:42 -0300

    contador

--- src/components/fragments/ItemListContainer/ItemListContainer.jsx
+++ src/components/fragments/ItemListContainer/ItemListContainer.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import './ItemListContainer.css';
-import './Counter';
+import CounterCustomhook from '../ItemCount/ItemCount';
+import '../ItemCount/ItemCount.css'
 
-
-export default function ItemListContainer({}) {
+export default function ItemListContainer() {
     function onAdd(count){
         console.log(`Llevas ${count} cocinitas`)
     }
     return(
         <div>
-            <Counter stock={10} onAdd={onAdd} />
+            <CounterCustomhook stock={10} onAdd={onAdd}/>
         </div>
         
     );
